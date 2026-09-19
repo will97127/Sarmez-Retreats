58a6b9aa4ee39d5439ca297dbcf3fb16d9d4cf87
will97127 <willpapo79@gmail.com>
2026-09-18 20:19:26 -0400

    Add Firebase configuration and Firestore initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB-pNaOwoA7cgKe3DM6Di0L_EBD3v_h2O4",
+  authDomain: "sarmez-retreats.firebaseapp.com",
+  databaseURL: "https://sarmez-retreats-default-rtdb.firebaseio.com",
+  projectId: "sarmez-retreats",
+  storageBucket: "sarmez-retreats.firebasestorage.app",
+  messagingSenderId: "586728433767",
+  appId: "1:586728433767:web:567f3000153393b13c8cc2",
+  measurementId: "G-ZG0YTEEGHP"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
